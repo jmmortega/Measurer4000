3f4b25465fd297b422f76ff57782b37a52f678a2
DiegoFaFe <diegofafe@gmail.com>
2017-06-16 12:24:22 +0200

    MAC tsted

--- Measurer4000.core/Utils/ShareCodeReportUtils.cs
+++ Measurer4000.core/Utils/ShareCodeReportUtils.cs
@@ -4,13 +4,7 @@ namespace Measurer4000.Core.Utils
 {
     public static class ShareCodeReportUtils
     {
-        private const string BASEURL = @"https://docs.google.com/forms/d/e/1FAIpQLSe1CMNFNnAh_GoZ3z9PD7d5a07CUd9zOVk3sywURY__zHMytA/viewform?
-                                       entry.187884389={0} 
-                                       &entry.1649909781={1}
-                                       &entry.1981106453={2}
-                                       &entry.827223542={3}
-                                       &entry.1102590771={4}
-                                       &entry.2068980640&entry.767914800&entry.1673597349&entry.1583496322";
+        private const string BASEURL = @"https://docs.google.com/forms/d/e/1FAIpQLSe1CMNFNnAh_GoZ3z9PD7d5a07CUd9zOVk3sywURY__zHMytA/viewform?entry.187884389={0}&entry.1649909781={1}&entry.1981106453={2}&entry.827223542={3}&entry.1102590771={4}&entry.2068980640&entry.767914800&entry.1673597349&entry.1583496322";
 
 
         public static string CreateShareUrl(CodeStats stats)
--- Measurer4000.mac/Services/WebBrowserMacTaskService.cs
+++ Measurer4000.mac/Services/WebBrowserMacTaskService.cs
@@ -2,6 +2,7 @@
 using Foundation;
 using Measurer4000.Core.Services.Interfaces;
 
+
 namespace Measurer4000.mac.Services
 {
     public class WebBrowserMacTaskService : IWebBrowserTaskService
