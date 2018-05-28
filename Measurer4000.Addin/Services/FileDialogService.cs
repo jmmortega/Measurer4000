e8d76a46f6e0efee8476b6ec4b1d243095132442
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-05-28 06:21:11 +0200

    Visual Studio for macOS Addin project (#16)
    * Added WPF Charts
    
    * Fixed WPF Charts color
    
    * Updated documentation. Added gif that quickly shows what the tool does.
    
    * Added MonoDevelop Add-in project

---
+++ Measurer4000.Addin/Services/FileDialogService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Measurer4000.Core.Models;
+using Measurer4000.Core.Services.Interfaces;
+using MonoDevelop.Components;
+using MonoDevelop.Ide;
+using Xwt;
+
+namespace Measurer4000.Addin.Services
+{
+    public class FileDialogService : IDialogService
+    {
+        public void CreateDialog(EnumTypeDialog type, string text, string title = "")
+        {
+            switch (type)
+            {
+                case EnumTypeDialog.Information:
+                    MessageService.ShowMessage(title, text);
+                    break;
+                case EnumTypeDialog.Warning:
+                    MessageService.ShowWarning(title, text);
+                    break;
+                default:
+                    MessageService.ShowMessage(title, text);
+                    break;
+            }
+        }
+
+        public void OpenFileDialog(Action<string> onFileDialogSuccess, Action<Exception> onFileDialogError)
+        {
+            using (var fileDialog = new Xwt.SelectFolderDialog("Browse For Solution"))
+            {
+                if (fileDialog.Run())
+                {
+                    onFileDialogSuccess?.Invoke(fileDialog.CurrentFolder);
+                }
+            }
+        }
+    }
+}
---
+++ Measurer4000.Addin/Windows/MeasurerWindow.cs
@@ -0,0 +1,21 @@
+﻿using Measurer4000.Addin.Views;
+using Xamarin.Forms.Platform.GTK.Extensions;
+
+namespace Measurer4000.Addin.Windows
+{
+    public class MeasurerWindow: Gtk.Window
+    {
+        public MeasurerWindow()
+            : base(Gtk.WindowType.Toplevel)
+        {
+            Title = "Measurer4000";
+            WindowPosition = Gtk.WindowPosition.Center;
+
+            var page = new MeasurerView();
+
+            Add(page.CreateContainer());
+
+            SetSizeRequest(800, 480);
+        }
+    }
+}
